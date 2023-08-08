932cd2b02b595e77b22d81b25c9b7bcf4e4c4c09
koffiemmal <messanjunior08@gmail.com>
2023-08-08 10:34:36 +0000

    les variables et declaration

---
+++ partie1/index.js
@@ -0,0 +1,6 @@
+let Nom = prompt("Quelle est votre Nom");
+console.log(Nom);
+let Prenom = prompt("Quelle est votre prenom");
+console.log(Prenom);
+
+console.log("Bienvenue " + Nom + " " + Prenom);
